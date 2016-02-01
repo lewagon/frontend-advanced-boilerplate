bfab4a92df175c29a8e60ca8b5b0ee05e1b7af5e
Sebastien Saunier <sebastien.saunier@gmail.com>
2016-02-01 10:11:37 +0100

    Disable livereload

--- config.rb
+++ config.rb
@@ -39,9 +39,9 @@ activate :autoprefixer
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
+# configure :development do
+#   activate :livereload
+# end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
